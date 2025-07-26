da25063b44c1414687db34a700f1288e1432e61f
chr14380 <chr14380@esri.com>
2025-07-25 23:26:47 -0700

    basic UI features
    Created BathroomCard component, separated styles, implemented recenter button

--- number-two/src/App.jsx
+++ number-two/src/App.jsx
@@ -1,54 +1,96 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./index.css";
+import "./BathroomCard.jsx"
 // Individual imports for each component used in this sample
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-zoom";
 import "@arcgis/map-components/components/arcgis-legend";
 import "@arcgis/map-components/components/arcgis-search";
+import "@arcgis/map-components/components/arcgis-locate";
+
 
 // Core API import
 import Graphic from "@arcgis/core/Graphic.js";
+import BathroomCard from "./BathroomCard.jsx";
 
 function App() {
+  const defaultCenter = [-73.9856644, 40.7484405]; 
+  const defaultZoom = 10;
+
+  const [viewPoint, setViewPoint] = useState(null); // Viewpoint set state
+  const [addPin, setAddPin] = useState(false);
+
+  const locateRef = useRef(null); // Reference to locate widget
+
   const handleViewReady = (event) => {
     const viewElement = event.target;
+    setViewPoint(viewElement);
+    const view = viewElement.arcgisView;
+  };
 
-    const point = {
-      type: "point",
-      longitude: -73.9856644,
-      latitude: 40.7484405,
-    };
+  const recenterMap = () => {
+    if (viewPoint) {
+      viewPoint.goTo({
+        center: defaultCenter,
+        zoom: defaultZoom
+      })
+    }
+  }
 
-    const markerSymbol = {
-      type: "simple-marker",
-      style: "triangle",
-      size: 15,
-      color: "red",
-      outline: {
-        color: "white",
-        width: 2,
-      },
-    };
+  // Override default goto behavior
+  useEffect(() => {
+    if(locateRef.current){
+      locateRef.current.goToOverride = (view, options) => {
+        options.target.scale = 1500;
+        return view.goTo(options.target);
+      };
+    }
+  }, []);
 
-    const pointGraphic = new Graphic({
-      geometry: point,
-      symbol: markerSymbol,
-    });
-
-    viewElement.graphics.add(pointGraphic);
+  const enableAddPinMode = () => {
+    setAddPin(true);
   };
 
   return (
-    <arcgis-map
-      item-id="5fcd777290d04ea2bcaa94a77374eeca"
-      onarcgisViewReadyChange={handleViewReady}
-      center="-73.9856644, 40.7484405"
-      zoom="10"
-    >
-      <arcgis-zoom position="top-left" />
-      <arcgis-search position="top-right" />
-      <arcgis-legend position="bottom-left" />
-    </arcgis-map>
+    <div className="content">
+      <h1 className="title">Gotta Go</h1>
+      <div className="navbar">
+        <button onClick={() => recenterMap()} className="recenter-button">
+          Recenter
+        </button>
+        <button onClick={() => enableAddPinMode} className="pin-button">
+          Toggle Pin Mode
+        </button>
+      </div>
+      <div className="cards">
+        <BathroomCard
+          title="TEST CARD"
+          description="TEST DESCRIPTION BLAH BLAH"
+          location={[0,0,0]}
+        />
+        <BathroomCard
+          title="TEST CARD"
+          description="TEST DESCRIPTION BLAH BLAH"
+          location={[0,0,0]}
+        />
+        <BathroomCard
+          title="TEST CARD"
+          description="TEST DESCRIPTION BLAH BLAH"
+          location={[0,0,0]}
+        />
+      </div>
+      <div className="map-container">
+        <arcgis-map
+          item-id="5fcd777290d04ea2bcaa94a77374eeca"
+          onarcgisViewReadyChange={handleViewReady}
+          center={defaultCenter.join(",")}
+          zoom={defaultZoom.toString()}
+        >
+          <arcgis-zoom position="top-left" />
+          <arcgis-locate ref={locateRef} position="top-left" />
+        </arcgis-map>
+      </div>
+    </div> 
   );
 }
 
