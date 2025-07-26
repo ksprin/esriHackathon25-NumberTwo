3ccb638affa14b5b3e8084d4e9fdc3a0be5cf811
Kayla Sprincis <ksprincis@esri.com>
2025-07-26 10:44:09 -0400

    Click function working

--- number-two/src/App.jsx
+++ number-two/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./index.css";
-import "./BathroomCard.jsx"
+import "./BathroomCard.jsx";
 // Individual imports for each component used in this sample
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-zoom";
@@ -8,38 +8,58 @@ import "@arcgis/map-components/components/arcgis-legend";
 import "@arcgis/map-components/components/arcgis-search";
 import "@arcgis/map-components/components/arcgis-locate";
 
-
 // Core API import
+import Map from "@arcgis/core/Map.js";
+import MapView from "@arcgis/core/views/MapView.js";
+import View from "@arcgis/core/views/View.js";
 import Graphic from "@arcgis/core/Graphic.js";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js";
 import BathroomCard from "./BathroomCard.jsx";
 
 function App() {
-  const defaultCenter = [-73.9856644, 40.7484405]; 
+  //const viewElement = document.querySelector("arcgis-map");
+
+  const defaultCenter = [-73.9856644, 40.7484405];
   const defaultZoom = 10;
+
+  let clickPoint; // Clicked Point on Map
 
   const [viewPoint, setViewPoint] = useState(null); // Viewpoint set state
   const [addPin, setAddPin] = useState(false);
 
   const locateRef = useRef(null); // Reference to locate widget
 
+  // Graphic Layer for pin to live on
+  let pointLayer = new GraphicsLayer({
+    id: "pointLayer",
+  });
+
   const handleViewReady = (event) => {
     const viewElement = event.target;
-    setViewPoint(viewElement);
-    const view = viewElement.arcgisView;
+    //setViewPoint(viewElement);
+    //const view = viewElement.arcgisView;
+    viewElement.map.add(pointLayer); // Layer for point
+
+    viewElement.addEventListener("arcgisViewClick", async (event) => {
+      clearGraphics();
+      clickPoint = event.detail.mapPoint;
+      // Pass point to the showPlaces() function
+      clickPoint && placePoint(clickPoint);
+    });
   };
 
   const recenterMap = () => {
     if (viewPoint) {
       viewPoint.goTo({
         center: defaultCenter,
-        zoom: defaultZoom
-      })
+        zoom: defaultZoom,
+      });
     }
-  }
+  };
 
   // Override default goto behavior
   useEffect(() => {
-    if(locateRef.current){
+    if (locateRef.current) {
       locateRef.current.goToOverride = (view, options) => {
         options.target.scale = 1500;
         return view.goTo(options.target);
@@ -50,6 +70,46 @@ function App() {
   const enableAddPinMode = () => {
     setAddPin(true);
   };
+
+  // Clear Pin Layer
+  function clearGraphics() {
+    pointLayer.removeAll(); // Remove graphics from GraphicsLayer
+  }
+
+  // Event Listener for dropping pin
+  const handleClick = async (event) => {
+    clearGraphics();
+    clickPoint = event.detail.mapPoint;
+    // Pass point to the showPlaces() function
+    clickPoint && placePoint(clickPoint);
+  };
+
+  // Place point based on click
+  async function placePoint(click) {
+    // Graphic of pin
+    //const point = {
+    //  type: "point",
+    //  center: click,
+    //};
+
+    const markerSymbol = {
+      type: "simple-marker",
+      style: "triangle",
+      size: 15,
+      color: "red",
+      outline: {
+        color: "white",
+        width: 2,
+      },
+    };
+
+    const pointGraphic = new Graphic({
+      geometry: click,
+      symbol: markerSymbol,
+    });
+
+    pointLayer.graphics.add(pointGraphic);
+  }
 
   return (
     <div className="content">
@@ -66,23 +126,24 @@ function App() {
         <BathroomCard
           title="TEST CARD"
           description="TEST DESCRIPTION BLAH BLAH"
-          location={[0,0,0]}
+          location={[0, 0, 0]}
         />
         <BathroomCard
           title="TEST CARD"
           description="TEST DESCRIPTION BLAH BLAH"
-          location={[0,0,0]}
+          location={[0, 0, 0]}
         />
         <BathroomCard
           title="TEST CARD"
           description="TEST DESCRIPTION BLAH BLAH"
-          location={[0,0,0]}
+          location={[0, 0, 0]}
         />
       </div>
       <div className="map-container">
         <arcgis-map
           item-id="5fcd777290d04ea2bcaa94a77374eeca"
           onarcgisViewReadyChange={handleViewReady}
+          arcgisViewClick={handleClick}
           center={defaultCenter.join(",")}
           zoom={defaultZoom.toString()}
         >
@@ -90,7 +151,7 @@ function App() {
           <arcgis-locate ref={locateRef} position="top-left" />
         </arcgis-map>
       </div>
-    </div> 
+    </div>
   );
 }
 
